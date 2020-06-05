24ab3242173f2d2b665d6ab3719df65a4fc95e8d
QUYEN NQUYEN <quyenpin09@gmail.com>
2020-06-05 10:11:56 +0700

    Updates

---
+++ precache-manifest.af014cfb57329cb96d7e84e7c5314c3e.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f9a53fb516c61257dce0c944fe3bb29",
+    "url": "/react2020/index.html"
+  },
+  {
+    "revision": "7a14949a6c303fe1372d",
+    "url": "/react2020/static/css/2.cf796be5.chunk.css"
+  },
+  {
+    "revision": "31b0514259f9219ca5b4",
+    "url": "/react2020/static/css/3.cf796be5.chunk.css"
+  },
+  {
+    "revision": "3d6db9128f52c1f7b9e4",
+    "url": "/react2020/static/css/5.d34346ea.chunk.css"
+  },
+  {
+    "revision": "f5818f83c9ad6a64189e",
+    "url": "/react2020/static/css/8.cf796be5.chunk.css"
+  },
+  {
+    "revision": "1e6b515734a8044bb3c0",
+    "url": "/react2020/static/js/0.3906f65a.chunk.js"
+  },
+  {
+    "revision": "56c21dbe1d9f8afd29a3",
+    "url": "/react2020/static/js/1.e635dce9.chunk.js"
+  },
+  {
+    "revision": "bc01ff94cb218bc58200",
+    "url": "/react2020/static/js/10.91b74192.chunk.js"
+  },
+  {
+    "revision": "23205cef38de40626a64",
+    "url": "/react2020/static/js/13.12ba6346.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react2020/static/js/13.12ba6346.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a14949a6c303fe1372d",
+    "url": "/react2020/static/js/2.6593388f.chunk.js"
+  },
+  {
+    "revision": "31b0514259f9219ca5b4",
+    "url": "/react2020/static/js/3.ec3a2c45.chunk.js"
+  },
+  {
+    "revision": "c6563e9130f9601f2318",
+    "url": "/react2020/static/js/4.dfa764d6.chunk.js"
+  },
+  {
+    "revision": "3d6db9128f52c1f7b9e4",
+    "url": "/react2020/static/js/5.1c6b033c.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/react2020/static/js/5.1c6b033c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8df333e1575e16c6442d",
+    "url": "/react2020/static/js/6.7ac9f781.chunk.js"
+  },
+  {
+    "revision": "3e28c75cb37a7369c2c0",
+    "url": "/react2020/static/js/7.487921f5.chunk.js"
+  },
+  {
+    "revision": "f5818f83c9ad6a64189e",
+    "url": "/react2020/static/js/8.09025a87.chunk.js"
+  },
+  {
+    "revision": "a6ee7c8b919ab3356c1a",
+    "url": "/react2020/static/js/9.88540db7.chunk.js"
+  },
+  {
+    "revision": "3b765fb2b3daa86d7b9c",
+    "url": "/react2020/static/js/main.9ae37937.chunk.js"
+  },
+  {
+    "revision": "e24d0a0e4698d0e09bd6",
+    "url": "/react2020/static/js/runtime-main.a68a2214.js"
+  }
+]);
